23c0577f1c658ddc307deaf363c1bf2d8ff37bf9
Musa V. Ahmed <mahme012@fiu.edu>
2015-02-02 01:58:48 -0500

    mn console test

--- symbiosim-study/src/iperftest.py
+++ symbiosim-study/src/iperftest.py
@@ -1,33 +1,43 @@
 #!/usr/bin/python
 
-from mininet.net import Mininet
+from mininet.net  import Mininet
 from mininet.topo import SingleSwitchTopo
+from mininet.cli  import CLI
+
+import threading
 import time
 import sys
 
+def startConsole( tid, net ):
+    cli = CLI
+    cli( net )
+    sys.stdout = sys.__stdout__
+    sys.stdin  = sys.__stdin__
+
 if __name__ == '__main__':
-    mytopo = SingleSwitchTopo(3)
-    net = Mininet(topo=mytopo)
+    mytopo = SingleSwitchTopo( 2 )
+    net = Mininet( topo=mytopo )
 
     net.start()
-
-    h1, h2 = net.getNodeByName('h1', 'h2')
     
-    h1log = open('h1.log', 'w')
-    h2log = open('h2.log', 'w')
+    thread1 = threading.Thread( target=startConsole, args=( 1, net ) )
+    thread1.start()
+
+    h1, h2 = net.getNodeByName( 'h1', 'h2' )
+    
+    h1log = open( 'h1.log', 'w' )
+    h2log = open( 'h2.log', 'w' )
 
     popens = {}
-    popens[1] = h1.popen("iperf -s",)
-    popens[2] = h2.popen("iperf -c %s -t 10" % h1.IP())
+    popens[1] = h1.popen( "iperf -s" )
+    popens[2] = h2.popen( "iperf -c %s -t 10" % h1.IP() )
 
-    time.sleep(1)
+    popens[3] = h1.popen( ["./ss_test.sh"], stdout=h1log )
+    popens[4] = h2.popen( ["./ss_test.sh"], stdout=h2log )
 
-    popens[3] = h1.popen(["./ss_test.sh"], stdout=h1log)
-    popens[4] = h2.popen(["./ss_test.sh"], stdout=h2log)
+    #out = popens[4].communicate()
 
-    out = popens[4].communicate()
-
-    time.sleep(10)
+    time.sleep( 10 )
 
     popens[1].terminate()
     popens[2].terminate()
@@ -37,5 +47,7 @@ if __name__ == '__main__':
 
     h1log.flush()
     h2log.flush()
+    
+    thread1.join()
 
     net.stop() 
