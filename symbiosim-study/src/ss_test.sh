d78985c3ba3185dff54d29727e3463fb7025a592
Musa V.Ahmed <mahme012@fiu.edu>
2015-02-03 16:59:07 -0500

    tcp_probe readme

--- symbiosim-study/src/ss_test.sh
+++ symbiosim-study/src/ss_test.sh
@@ -3,5 +3,5 @@ for i in {1..20}
 do
     TIMESTAMP=$(date "+%H:%M:%S:%N")
     ss --tcp | awk -v "ts=$TIMESTAMP" '{if (NR!=1) {printf "%-20d\t%-20d\t%-20s\t%-20s\t%-20s\n", $2, $3, $4, $5, ts}}'
-    sleep 1s
+    sleep 0.01s
 done
