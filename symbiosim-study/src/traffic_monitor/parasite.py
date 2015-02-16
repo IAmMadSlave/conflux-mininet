f64bf6dd823ccf1f92b3fdcd620683b5cb0c0085
Musa V. Ahmed <mahme012@fiu.edu>
2015-02-16 00:36:12 -0500

    sun night

--- symbiosim-study/src/traffic_monitor/parasite.py
+++ symbiosim-study/src/traffic_monitor/parasite.py
@@ -1,9 +1,31 @@
-#!/bin/bash
+#!/bin/env python
 
-import os.path
-import subprocess.call
+import logging
+import os
+import subprocess
+import time
 
-if not os.path.exists( './tempdisk' ):
-    os.makedirs( '.tempdisk' )
+try:
+    logging.info( 'unload kernel module' )
+    subprocess.call( ['modprobe', '-r', 'tcp_probe'] )
 
-call( ["", "", "", ] )
+    logging.info( 'load kernel module' )
+    subprocess.call( ['modprobe', 'tcp_probe', 'full=1', 'bufsize=512'] )
+
+    logging.info( 'change permissions to read on tcpprobe output' )
+    subprocess.call( ['chmod', '444', '/proc/net/tcpprobe'] )
+
+    with open( '/proc/net/tcpprobe' ) as f:
+        for i in range(1, 10):
+            flag = True
+            while flag:
+                f.readline()
+            print f.readline()
+
+    logging.info( 'close devnull' )
+    devnull.close()
+
+except IOError as e:
+    if( e[0] == errno.EPERM ):
+        print >> sys.stderr, "Please run with root permissions"
+        sys.exit(1)
