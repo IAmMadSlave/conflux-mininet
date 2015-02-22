3ef9f79032c75be034348563cf267b7eb9a45d5b
Musa V. Ahmed <mahme012@fiu.edu>
2015-02-22 00:29:41 -0500

    sat

--- symbiosim-study/src/downscaler/downscaler.py
+++ symbiosim-study/src/downscaler/downscaler.py
@@ -1,15 +1,32 @@
 #!/bin/python
-import networkx as nx
 
 try:
     import xml.etree.cElementTree as ET
+    import networkx as nx
+    import pprint
 except ImportError:
     import xml.etree.ElementTree as ET
+
+
+def getName( node ):
+    return node.attrib.get( 'name' )
+
+def getPath( node ):
+    return node.attrib.get( 'path' )
+
+def getType( node ):
+    return node.attrib.get( 'type' )
+
+def getTag( node ):
+    return node.tag
+
+def getValue( node ):
+    return node.attrib.get( 'value' )
 
 net = {}
 
 # parse <model>
-tree = ET.ElementTree(file='dumbbell.xml')
+tree = ET.ElementTree(file='linear.xml')
 root = tree.getroot()
 
 # add topNet to net{} -> { 'topNet': {} }
@@ -18,6 +35,7 @@ for child in root:
     root = child
 
 # for each net in topNet update net{} -> { 'topNet': {'sub1': {}, 'sub2': {} } }
+'''
 for child in root:
     if child.attrib.get('type') == 'Net':
         net['topNet'].update({child.attrib.get('name'): {}})
@@ -29,7 +47,7 @@ for child in root:
                     if subsubchild.attrib.get('type') == 'Interface':
                         net['topNet'][child.attrib.get('name')][subchild.attrib.get('type')]['interfaces'].update(
                                 {'name': subsubchild.attrib.get('name')} )
-                
+
             if subchild.attrib.get('type') == 'Router':
                 break; 
 
@@ -38,7 +56,60 @@ for child in root:
 
     if child.tag == 'replica':
         net['topNet'].update({child.attrib.get('name'): {}})
+'''
+
+#net['topNet'].update( {'subnets': [{}] }  )
+topNet = net['topNet']
+hasSubnets = False
+subnets = []
+hasReplicas = False
+replicas = []
+
+hosts = []
+routers = []
+links = []
+
+for child in root:
+    if getName( child ) == 'routing':
+        topNet.update( {'routing': getType( child )} )
+
+    if hasSubnets == False and getType( child ) == 'Net':
+        topNet.update( {'subnets': [{}]} )
+        hasSubnets = True
+
+    if getTag( child ) == 'replica':
+        replicas.append( (getPath( child ), getName( child )) ) 
+        hasReplicas = True
+
+    if getType ( child ) == 'Host':
+        newHost = {'name': getName( child )} 
+        newHost.update( {'interfaces': []} )
+        for interface in child:
+            newHost['interfaces'].append( {'name': getName( interface )} )
+        hosts.append( newHost )    
+
+    if getType ( child ) == 'Router':
+        newRouter = {'name': getName( child )}
+        newRouter.update( {'interfaces': []} )
+        for interface in child:
+            newRouter['interfaces'].append( {'name': getName( interface )} )
+        routers.append( newRouter )
+
+    if getType ( child ) == 'Link':
+        newLink = {'name': getName( child )}
+        linkPath = ''
+        for subchild in child:
+            if getTag( subchild ) == 'attribute':
+                newLink.update( {getName( subchild ): getValue( subchild )} )
+
+            if getTag( subchild ) == 'ref':
+                linkPath += getPath( subchild )
+
+        newLink.update( {'path': linkPath} )
+        links.append( newLink)
 
 
-print net
-print net['topNet']['sub1']
+#print net
+pprint.pprint( hosts )
+pprint.pprint( routers )
+pprint.pprint( links )
