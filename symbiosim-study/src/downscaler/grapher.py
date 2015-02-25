382b5fd79a9f57448860f64d923e7d32832ac621
Musa V. Ahmed <mahme012@fiu.edu>
2015-02-25 11:35:04 -0500

    wed

--- symbiosim-study/src/downscaler/grapher.py
+++ symbiosim-study/src/downscaler/grapher.py
@@ -7,59 +7,90 @@ import networkx as nx
 
 import matplotlib.pyplot as plt
 
+def routing_type( top ):
+    return top['routing']
+    
 def str_to_num( str ):
     try:
         return int( str )
     except ValueError:
         return float( str )
 
+def has_subnets( top ):
+    subnets = None
+    if top.has_key( 'subnets' ):
+        subnets = []
+        for subnet in top['subnets']:
+            subnets.append( {'name': subnet['name'], 'nodes': []} )
+    return subnets
+
+def json_to_networkx( g, top, s=None ):
+    if top.has_key( 'hosts' ):
+        for host in top['hosts']:
+            g.add_node( host['name'], interfaces=len( host['interfaces'] ) )
+            for interface in host['interfaces']:
+                g.add_node( (host['name'], interface['name']),
+                    bit_rate=str_to_num( interface['bit_rate'] ), 
+                    latency=str_to_num( interface['latency'] ))
+                g.add_edge( (host['name'], interface['name']), host['name'] )
+
+    if top.has_key( 'routers' ):
+        for router in top['routers']:
+            g.add_node( router['name'] )
+            for interface in router['interfaces']:
+                g.add_node( (router['name'], interface['name']),
+                    bit_rate=str_to_num( interface['bit_rate'] ),
+                    latency=str_to_num( interface['latency'] ) )
+                g.add_edge( (router['name'], interface['name']), router['name'] )
+
+    if top.has_key( 'links' ):
+        for link in top['links']:
+            path = link['path']
+            path = path.replace( '..', '', 1 )
+            path = path.split( '..' )
+            for i in range( len( path ) ):
+                path[i] = path[i].replace( ':', '', 1 )
+                path[i] = path[i].split( ':' )
+            g.add_edge( tuple( path[0] ), tuple( path[1] ),
+                bandwidth=str_to_num( link['bandwidth'] ), 
+                delay=str_to_num( link['delay'] ),
+                name=link['name'],)
+
 jsontopology = open( sys.argv[1] )
 net = json.load( jsontopology )
 net = ast.literal_eval( json.dumps(net) )
 
-g = nx.Graph()
+g = nx.Graph( routing=routing_type( net['topNet'] ) )
 
-if net['topNet'].has_key( 'hosts' ):
-    for host in net['topNet']['hosts']:
-        g.add_node( host['name'], interfaces=len( host['interfaces'] ) )
-        for interface in host['interfaces']:
-            g.add_node( (host['name'], interface['name']),
-                    bit_rate=str_to_num( interface['bit_rate'] ), 
-                    latency=str_to_num( interface['latency'] ))
-            g.add_edge( (host['name'], interface['name']), host['name'] )
+subnets = has_subnets( net['topNet'] )
 
-if net['topNet'].has_key( 'routers' ):
-    for router in net['topNet']['routers']:
-        g.add_node( router['name'] )
-        for interface in router['interfaces']:
-            g.add_node( (router['name'], interface['name']),
-                    bit_rate=str_to_num( interface['bit_rate'] ),
-                    latency=str_to_num( interface['latency'] ) )
-            g.add_edge( (router['name'], interface['name']), router['name'] )
+if subnets is None:
+    json_to_networkx(g, net['topNet'])
+else:
+    json_to_networkx(g, net['topNet'], subnets)
 
-if net['topNet'].has_key( 'links' ):
-    for link in net['topNet']['links']:
-        path = link['path']
-        path = path.replace( '..', '', 1 )
-        path = path.split( '..' )
-        for i in range( len( path ) ):
-            path[i] = path[i].replace( ':', '', 1 )
-            path[i] = path[i].split( ':' )
-        g.add_edge( tuple( path[0] ), tuple( path[1] ),
-        bandwidth=str_to_num( link['bandwidth'] ), 
-        delay=str_to_num( link['delay'] ),
-        name=link['name'],)
 
-#print '\n\n'
-#print 'NODES...'
-#for n in g.nodes(data=True):
-#    print n
+# adding nodes to subnets
+'''
+print 'BEFORE...'
+print subnets
 
-#print 'EDGES...'
-#print g.edges(data=True)
+for i in range( len( subnets ) ):
+    if subnets[i]['name'] == 'sub1':
+        subnets[i][''].append('h1')
 
-print 'SHORTEST PATH FROM H1 TO H2...'
-print nx.shortest_path( g, 'h1', 'h2', 'delay' )
+print '\nAFTER...'
+print subnets
+'''
+
+# shortest path calculation
+#print nx.shortest_path( g, 'h1', 'h2', 'delay' )
+
+# test
+print 'NODES...'
+print g.nodes()
+print '\nEDGES...'
+print g.edges()
 
 pos = nx.spectral_layout(g)
 nx.draw_networkx_nodes(g,pos,node_size=350)
@@ -68,4 +99,3 @@ nx.draw_networkx_labels(g,pos,font_size=10)
 
 plt.axis('off')
 plt.savefig("test.png")
-plt.show()
