0df5e6da9517634f17f7af71fa1691dd77ceb98d
Musa V. Ahmed <mahme012@fiu.edu>
2015-02-25 16:33:38 -0500

    wed before class

--- symbiosim-study/src/downscaler/grapher.py
+++ symbiosim-study/src/downscaler/grapher.py
@@ -25,37 +25,82 @@ def has_subnets( top ):
     return subnets
 
 def json_to_networkx( g, top, s=None ):
+    if top.has_key( 'subnets' ):
+        for subnet in top['subnets']:
+            json_to_networkx( g, subnet, s )
+
     if top.has_key( 'hosts' ):
         for host in top['hosts']:
-            g.add_node( host['name'], interfaces=len( host['interfaces'] ) )
+            if s is not None:
+                for i in range( len( s ) ):
+                    if subnets[i]['name'] == top['name']:
+                        nodename = top['name']+':'+host['name']
+                        subnets[i]['nodes'].append( nodename )
+                        break
+                    else:
+                        continue
+                    break
+            else:
+                nodename = host['name']
+            g.add_node( nodename, interfaces=len( host['interfaces'] ) )
             for interface in host['interfaces']:
-                g.add_node( (host['name'], interface['name']),
+                interfacename = nodename+':'+interface['name']
+                if s is not None and nodename.find( top['name'] ) > -1:
+                    subnets[i]['nodes'].append( interfacename )
+
+                g.add_node( interfacename,
                     bit_rate=str_to_num( interface['bit_rate'] ), 
                     latency=str_to_num( interface['latency'] ))
-                g.add_edge( (host['name'], interface['name']), host['name'] )
+                g.add_edge( interfacename, nodename )
 
     if top.has_key( 'routers' ):
         for router in top['routers']:
-            g.add_node( router['name'] )
+            if s is not None:
+                for i in range( len( s ) ):
+                    if subnets[i]['name'] == top['name']:
+                        nodename = top['name']+':'+router['name']
+                        subnets[i]['nodes'].append( nodename )
+                        break
+                    else:
+                        continue
+                    break
+            else:
+                nodename = router['name']
+            g.add_node( nodename )
             for interface in router['interfaces']:
-                g.add_node( (router['name'], interface['name']),
+                interfacename = nodename+':'+interface['name']
+                if s is not None and nodename.find( top['name'] ) > -1:
+                    subnets[i]['nodes'].append( interfacename )
+                    
+                g.add_node( interfacename,
                     bit_rate=str_to_num( interface['bit_rate'] ),
                     latency=str_to_num( interface['latency'] ) )
-                g.add_edge( (router['name'], interface['name']), router['name'] )
+                g.add_edge( interfacename, nodename )
 
     if top.has_key( 'links' ):
         for link in top['links']:
             path = link['path']
             path = path.replace( '..', '', 1 )
             path = path.split( '..' )
-            for i in range( len( path ) ):
-                path[i] = path[i].replace( ':', '', 1 )
-                path[i] = path[i].split( ':' )
-            g.add_edge( tuple( path[0] ), tuple( path[1] ),
+
+            if top != net['topNet'] and s is not None:
+                for i in range( len( s ) ):
+                    if subnets[i]['name'] == top['name']:
+                        for i in range( len( path ) ):
+                            path[i] = top['name']+':'+path[i]
+                        break
+                    else:
+                        continue
+                    break
+            else:
+                for i in range( len( path ) ):
+                    path[i] = path[i].replace( ':', '', 1)
+'''
+            g.add_edge( path[0], path[1],
                 bandwidth=str_to_num( link['bandwidth'] ), 
                 delay=str_to_num( link['delay'] ),
                 name=link['name'],)
-
+'''
 jsontopology = open( sys.argv[1] )
 net = json.load( jsontopology )
 net = ast.literal_eval( json.dumps(net) )
@@ -70,30 +115,20 @@ else:
     json_to_networkx(g, net['topNet'], subnets)
 
 
-# adding nodes to subnets
-'''
-print 'BEFORE...'
+print 'SUBNETS...'
 print subnets
-
-for i in range( len( subnets ) ):
-    if subnets[i]['name'] == 'sub1':
-        subnets[i][''].append('h1')
-
-print '\nAFTER...'
-print subnets
-'''
 
 # shortest path calculation
 #print nx.shortest_path( g, 'h1', 'h2', 'delay' )
 
 # test
-print 'NODES...'
+print '\nNODES...'
 print g.nodes()
 print '\nEDGES...'
 print g.edges()
 
-pos = nx.spectral_layout(g)
-nx.draw_networkx_nodes(g,pos,node_size=350)
+pos = nx.random_layout(g)
+nx.draw_networkx_nodes(g,pos,node_size=700)
 nx.draw_networkx_edges(g,pos,width=3)
 nx.draw_networkx_labels(g,pos,font_size=10)
 
