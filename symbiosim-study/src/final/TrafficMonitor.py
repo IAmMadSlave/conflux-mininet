2884def6d091342d1fafa9b958c5a443ca18bf5b
Musa V. Ahmed <mahme012@fiu.edu>
2015-03-07 21:48:21 -0500

    start to fix parser

---
+++ symbiosim-study/src/final/TrafficMonitor.py
@@ -0,0 +1,101 @@
+import re
+import os
+import sys
+import time
+import subprocess
+import threading
+from subprocess import Popen, PIPE
+from Queue import Queue, Empty
+
+ON_POSIX = 'posix' in sys.builtin_module_names
+
+def enqueue_output( out, queue ):
+    for line in iter( out.readline, b'' ):
+            queue.put( line )
+    out.close()
+
+class TrafficMonitor():
+    
+    def __init__( self, mn_pipes_file, demand_file ):
+        # for writing demand to file
+        self.demand_file = demand_file
+      
+        # read pipe info 
+        pipes = []
+        with open( mn_pipes_file, 'r') as openfile:
+            for line in openfile:
+                pipes.append( line )
+
+        i = 0
+        for i in range( len( pipes ) ):
+            pipes[i] = pipes[i].split( ' ' )
+           
+        # setup pipes in memory 
+        self.pipes_table = []
+        for p in pipes:
+            self.pipes_table.append( {'sim_dest': p[4].strip(), 'dest':
+                p[3].strip(), 'sim_src': p[2].strip(), 'src': p[1].strip(),
+                'nxt': 0, 'name':p[0].strip() } )
+
+    def run( self ):
+        # module loading 
+        self.start_module()
+        print 'start module'
+
+        cat = Popen( ['cat', '/proc/net/tcpprobe'], stdout=PIPE, bufsize=1,
+            close_fds=ON_POSIX )
+        q = Queue()
+        t1 = threading.Thread( target=enqueue_output, args=(cat.stdout, q) )
+        t1.daemon = True
+        t1.start()
+
+        t2 = threading.Thread( target=self.continous_update, args=(q,) )
+        t2.daemon = True
+        t2.start()
+       
+        t3 = threading.Thread( target=self.timed_update )
+        t3.start()
+
+        t3.join()
+        cat.terminate()
+        # module unloading
+        self.stop_module()
+        print 'mod stop'
+        return
+
+    def start_module( self ):
+        # unload kernel module
+        subprocess.call( ['modprobe', '-r', 'tcp_probe'] )
+
+        # load kernel module
+        subprocess.call( ['modprobe', 'tcp_probe', 'full=0'] )
+
+        # change permissions to read on tcp_probe output
+        subprocess.call( ['chmod', '444', '/proc/net/tcpprobe'] )
+
+    def stop_module( self ):
+        # unload kernel module
+        subprocess.call( ['modprobe', '-r', 'tcp_probe'] )
+
+    def continous_update( self, q ):
+        while True:
+            try:
+                line = q.get_nowait()
+            except Empty:
+                line = None
+            else:
+                lineparts = line.split( ' ' )
+                self.pipes_table[0]['nxt'] = lineparts[4]
+
+    def timed_update( self ):
+        # write periodic traffic demand to file
+        with open( self.demand_file, 'w') as demand:
+            # write demand once per second
+            i= 0
+            for i in range( 50 ):
+                for pipe in self.pipes_table:
+                    demand.write( pipe['sim_src']+' '+pipe['sim_dest']+' '+str(
+                        pipe['nxt'] ) +'\n' )
+                time.sleep(1)
+                # add drift control here
+            return
