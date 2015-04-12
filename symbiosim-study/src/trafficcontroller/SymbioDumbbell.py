c88f60493c4c5781092d298ffea830dd23d1181b
Musa V. Ahmed <mahme012@fiu.edu>
2015-04-12 16:00:17 -0400

    finally fixed tc bw and loss

--- symbiosim-study/src/trafficcontroller/SymbioDumbbell.py
+++ symbiosim-study/src/trafficcontroller/SymbioDumbbell.py
@@ -13,9 +13,9 @@ from mininet.cli  import CLI
 
 from TrafficMonitor import TrafficMonitor
 
-#sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-#server_address = ( 'localhost', 51717 )
-#sock.connect( server_address )
+sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+server_address = ( 'localhost', 51717 )
+sock.connect( server_address )
 #print ('SymbioDumbell: socket is %s' % sock)
 
 log_file = open( 'tc_changes.out', 'w' )
@@ -48,12 +48,20 @@ def wget_long( src, dest ):
     src.popen( ['wget', dest_ip] )
     return
 
-# iperf 
+# iperf2
 def iperf( src, dest, duration, interval ):
     iperf_server = dest.popen( 'iperf -s' )
     out, err, exitcode = src.pexec( 'iperf -c %s -t %s -i %s' % ( dest.IP(),
         duration, interval) )
     return out
+
+# iperf3
+def iperf3( src, dest, duration, interval ):
+    iperf_server = dest.popen( 'iperf3 -s' )
+    out, err, exitcode = src.pexec( 'iperf3 -c %s -t %s -i %s' % ( dest.IP(),
+        duration, interval) )
+    return out
+
 
 # functions for test_file (tcpprobe)
 def call_traffic_monitor():
@@ -66,15 +74,33 @@ def start_traffic_monitor():
     return
 
 # functions for tc changes
-def tc_change( host, bandwidth, drop_prob ):
-    cmd = 'tc qdisc replace dev %s root handle 1:0 htb default 10' % (host.defaultIntf(),)
-    host.popen( cmd )
-    cmd = 'tc class replace dev %s parent 1:0 classid 1:10 htb rate %smbit' % (host.defaultIntf(), bandwidth, )
-    host.popen( cmd )
-    if float( drop_prob ) > 0:
-        cmd = 'tc qdisc replace dev %s root netem loss %s%' %(host.defaultIntf(), drop_prob,)
+def setup_tc( host ):
+    cmd = 'tc -s qdisc ls dev %s' % ( host.defaultIntf(), )
+    out, err, exitcode = host.pexec( cmd )
+    if out.find( 'htb' ) < 0:
+        cmd = 'tc qdisc add dev %s handle 1:0 root htb default 10' % ( host.defaultIntf(), )
         host.popen( cmd )
-    log_file.write( str( datetime.datetime.now() ) + ' ' + str( bandwidth ) + '\n' )
+        return True
+    else:
+        return True
+
+def tc_change( host, bandwidth, drop_prob ):
+#    cmd = 'tc qdisc replace dev %s root handle 1:0 htb default 10' % (host.defaultIntf(),)
+#    host.popen( cmd )
+#    cmd = 'tc class replace dev %s parent 1:0 classid 1:10 htb rate %smbit' % (host.defaultIntf(), bandwidth, )
+#    host.popen( cmd )
+#    if float( drop_prob ) > 0:
+#        cmd = 'tc qdisc replace dev %s root netem loss %s%' %(host.defaultIntf(), drop_prob,)
+#        host.popen( cmd )
+    setup_tc( host )
+    cmd = 'tc class replace dev %s parent 1:0 classid 1:10 htb rate %smbit' % ( host.defaultIntf(), bandwidth, )
+    host.popen( cmd )
+    if float( drop_prob ) > 0.0:
+        drop_prob = float( drop_prob ) / 100.0
+        drop_prob = str( drop_prob )
+        cmd = 'tc qdisc replace dev %s parent 1:10 handle 10 netem loss %s%% delay 1ms' % ( host.defaultIntf(), drop_prob, )
+        host.popen( cmd )
+    log_file.write( str( datetime.now() ) + ' ' + str( bandwidth ) + ' ' + str( drop_prob ) +'\n' )
     return
 
 def tc_listener( hosts ):
@@ -97,10 +123,9 @@ def tc_listener( hosts ):
 
             for h in hosts:
                 for t in tc_changes:
-                    tc_change( h, t[1], t[2] )
+                    tc_change( h, t[2], t[1] )
 
             #tc_file.close()
-            time.sleep(0.001)
             continue
         #time.sleep(1)
     return
@@ -124,6 +149,7 @@ def main():
     l1 = net.addLink( h1, h2, cls=TCLink )
 
     net.start()
+    tc_change( h1, '10', '0.0' )
 
     # start tc lister here
     start_tc_listener( hosts )
@@ -133,13 +159,17 @@ def main():
     start_traffic_monitor()
 
     # add iperf 20s 1s interval
-    out = iperf( h1, h2, 20, 1 )
+    #out = iperf( h1, h2, 20, 1 )
+    out = iperf3( h1, h2, 20, 1 )
+    print out
+    
+    out, err, exitcode = h1.pexec( 'ping -c 5 10.0.0.2' )
     print out
 
-    set_server( h2 )
-    wget_short( h1, h2 )
-    wget_long( h1, h2 )
-    kill_server()
+    #set_server( h2 )
+    #wget_short( h1, h2 )
+    #wget_long( h1, h2 )
+    #kill_server()
 
     #cli = CLI
     #cli( net )
